27bbeb792666fbf7f2e6ae3dbaf0f54e5ee4a0c0
ajmain <ajmain@LAPTOP-RSN0DJMG>
2024-08-16 11:31:27 +0100

    confirmed connected database

--- pathpal/urls.py
+++ pathpal/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pathpalApp.views import create_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/create/', create_user, name="create_user")
 ]
--- pathpalApp/views.py
+++ pathpalApp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt 
+from pathpalApp.services.user_service import create_test_user
 
-# Create your views here.
+@csrf_exempt
+def create_user(request):
+    if request.method == "POST": 
+        data = request.POST
+        user_id = create_test_user(data)
+        return JsonResponse({"id": str(user_id)})
+    

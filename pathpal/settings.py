ed5f9a55f00e807dd87effabb94736ca0a176fa1
ajmain <ajmain@LAPTOP-RSN0DJMG>
2024-08-16 10:12:15 +0100

    FIX ERRORS

--- pathpal/settings.py
+++ pathpal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default="", cast=lambda v: [s.strip() for s in v.split(',')])
 
 # MongoDB settings
 MONGO_DB_NAME = config('DB_NAME')
--- pathpalApp/apps.py
+++ pathpalApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PathpallappConfig(AppConfig):
+class PathpalappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pathpallApp'
+    name = 'pathpalApp'

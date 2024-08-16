babf29b8b33985d5113ce89bfdd9d983eb310a2f
Daniel Taylor <danieltaylor@Daniels-MacBook-Pro.local>
2024-08-16 12:13:31 +0100

    configured mongodb connection with atlas

--- pathpalApp/views.py
+++ pathpalApp/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt 
 from pathpalApp.services.user_service import create_test_user
+from django.conf import settings
 
 @csrf_exempt
 def create_user(request):
@@ -9,3 +10,10 @@ def create_user(request):
         user_id = create_test_user(data)
         return JsonResponse({"id": str(user_id)})
     
+def test_mongo_connection(request):
+    try:
+        collections = settings.MONGO_DB.list_collection_names()
+        return JsonResponse({"status": "success", "collections": collections})
+    except Exception as e:
+        return JsonResponse({"status": "error", "msg": str(e)})
+        

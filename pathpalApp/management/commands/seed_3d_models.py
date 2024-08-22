03943a69ef7ece240743af2604bd3f65d1a0f6db
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-08-22 10:15:51 +0100

    updated 3d moodel structure

--- pathpalApp/management/commands/seed_3d_models.py
+++ pathpalApp/management/commands/seed_3d_models.py
@@ -1,7 +1,6 @@
 import os
 from django.core.management.base import BaseCommand
 from pymongo import MongoClient
-from gridfs import GridFS
 from django.conf import settings
 from pathpalApp.models.three_d_models import ThreeDModel
 from pathpalApp.serializers import ThreeDModelSerializer
@@ -19,41 +18,31 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         client = MongoClient(settings.MONGO_URI)
         db = client[settings.MONGO_DB_NAME]
-        fs = GridFS(db)
+
 
         if options['clear']:
             self.stdout.write(self.style.WARNING('Clearing existing 3D models...'))
             db.three_d_models.delete_many({})
-            fs.delete()
-
-        def upload_file_to_gridfs(file_path):
-            with open(file_path, 'rb') as file_data:
-                return fs.put(file_data, filename=os.path.basename(file_path))
-
-        models_folder = 'test_3d_files'
         
-        
-        for filename in os.listdir(models_folder):
-            if filename.endswith('.glb'):
+        if options['file']:
+            json_file_path = options['file']
+        else:
+            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            json_file_path = os.path.join(base_dir, 'pathpalApp','three_d_models.json')
+
+        try:
+            with open(json_file_path,'r') as file:
+                models_data=json.load(file)
+            for model_data in models_data:
                 try:
-                    name = os.path.splitext(filename)[0]
-                    glb_file_path = os.path.join(models_folder, filename)
-                    
-                    glb_id = upload_file_to_gridfs(glb_file_path)
-                    
-                    
-                    model = ThreeDModel(
-                        name=name,
-                        glb_id=glb_id,
-                    )
-                    
-                    serialized_data = ThreeDModelSerializer(model).data
-                    
-                    result = db.three_d_models.insert_one(serialized_data)
-                    self.stdout.write(self.style.SUCCESS(f"Inserted {name} with ID: {result.inserted_id}"))
-                    
+                    model=ThreeDModel.from_dict(model_data)
+                    serialized_data=ThreeDModelSerializer(model).data
+                    result=db.three_d_models.insert_one(serialized_data)
+                    self.stdout.write(self.style.SUCCESS(f'Inserted{model.name}'))
                 except Exception as e:
-                    self.stdout.write(self.style.ERROR(f"Error processing {filename}: {str(e)}"))
+                    self.stdout.write(self.style.ERROR(f'Error inserting model{model.name}'))
+        except Exception as e:
+                    self.stdout.write(self.style.ERROR(f'Error reading json file'))
         
         client.close()
         self.stdout.write(self.style.SUCCESS('Successfully seeded 3D models'))
